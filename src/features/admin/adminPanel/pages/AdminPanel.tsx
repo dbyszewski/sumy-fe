6f554f4f1263e40ab0cff5802844a147d789da48
Toskyuu <robert.laski10@gmail.com>
2024-05-13 22:26:36 +0200

    wykasowanie komentarza

--- src/features/admin/adminPanel/pages/AdminPanel.tsx
+++ src/features/admin/adminPanel/pages/AdminPanel.tsx
@@ -3,7 +3,6 @@ import { AdminTable } from '@/components/Elements/Table/AdminTable.tsx';
 export const AdminPanel = () => {
   return (
     <div>
-      {/*<AdminTable data={data} />*/}
       <AdminTable />
     </div>
   );
