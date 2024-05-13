604e9dd51deaa18f22210d92d2c36167560954f8
Toskyuu <robert.laski10@gmail.com>
2024-05-13 21:26:46 +0200

    Coś tam działa i pobiera z api :)

--- src/components/Elements/Table/AdminTable.tsx
+++ src/components/Elements/Table/AdminTable.tsx
@@ -1,6 +1,23 @@
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
+import { axios } from '@/lib/axios.ts';
 export const AdminTable = () => {
+  const [tableData, setTableData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('/home/events');
+        setTableData(response.data.result);
+      } catch (error) {
+        console.error('Błąd podczas pobierania danych:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <StyledTable>
       <thead>
@@ -8,9 +25,9 @@ export const AdminTable = () => {
           <th>Numer telefonu</th>
           <th>Nazwa użytkownika</th>
           <th>Tytuł</th>
+          <th>Opis</th>
           <th>Data wydarzenia</th>
           <th>Data zgłoszenia</th>
-          <th>Opis</th>
           <th>Status</th>
         </tr>
       </thead>
@@ -19,40 +36,40 @@ export const AdminTable = () => {
           <td>123 123 123</td>
           <td>Hymel Jadwiga</td>
           <td>Styrta się pali</td>
+          <td>Ja nie wiem kto podpalił</td>
           <td>2024-10-05 23:59:00</td>
           <td>2024-10-05 23:59:40</td>
-          <td>Ja nie wiem kto podpalił</td>
           <td>Jakis status</td>
         </tr>
         <tr>
           <td>123 123 123</td>
           <td>Hymel Jadwiga</td>
           <td>Styrta się pali</td>
+          <td>Ja nie wiem kto podpalił</td>
           <td>2024-10-05 23:59:00</td>
           <td>2024-10-05 23:59:40</td>
-          <td>Ja nie wiem kto podpalił</td>
           <td>Jakis status</td>
         </tr>
         <tr>
           <td>123 123 123</td>
           <td>Hymel Jadwiga</td>
           <td>Styrta się pali</td>
+          <td>Ja nie wiem kto podpalił</td>
           <td>2024-10-05 23:59:00</td>
           <td>2024-10-05 23:59:40</td>
-          <td>Ja nie wiem kto podpalił</td>
           <td>Jakis status</td>
         </tr>
-        {/*{data.map((row, index) => (*/}
-        {/*  <tr key={index}>*/}
-        {/*    <td>{row.phoneNumber}</td>*/}
-        {/*    <td>{row.username}</td>*/}
-        {/*    <td>{row.title}</td>*/}
-        {/*    <td>{row.eventDate}</td>*/}
-        {/*    <td>{row.reportDate}</td>*/}
-        {/*    <td>{row.description}</td>*/}
-        {/*    <td>{row.status}</td>*/}
-        {/*  </tr>*/}
-        {/*))}*/}
+        {tableData.map((row, index) => (
+          <tr key={index}>
+            <td>{row.phone}</td>
+            <td>{row.userName}</td>
+            <td>{row.title}</td>
+            <td>{row.description}</td>
+            <td>{row.eventDate}</td>
+            <td>{row.reportDate}</td>
+            <td>{row.status}</td>
+          </tr>
+        ))}
       </tbody>
     </StyledTable>
   );
