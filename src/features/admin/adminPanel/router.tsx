0a17deb46aa2329e4d1a85facbdc1570fa46d1ad
Toskyuu <robert.laski10@gmail.com>
2024-05-16 22:16:24 +0200

    zmiana dateHelpera, żeby w sytuacji nulla zwracał pusty string zamiast aktualnej daty

--- src/features/admin/adminPanel/router.tsx
+++ src/features/admin/adminPanel/router.tsx
@@ -3,6 +3,8 @@ import { Route, Routes } from 'react-router-dom';
 import { Layout } from './components/Layout.tsx';
 import { AdminPanelEvents } from './pages/AdminPanelEvents.tsx';
 
+import { AdminPanelUsers } from '@/features/admin/adminPanel/pages/AdminPanelUsers.tsx';
+
 export const AdminRouter = () => {
   return (
     <Routes>
@@ -12,7 +14,7 @@ export const AdminRouter = () => {
         children={
           <>
             <Route path="/events" element={<AdminPanelEvents />} />
-            <Route path="/users" element={<AdminPanelEvents />} />
+            <Route path="/users" element={<AdminPanelUsers />} />
             <Route path="*" element={<h1>404-xd</h1>} />
           </>
         }
