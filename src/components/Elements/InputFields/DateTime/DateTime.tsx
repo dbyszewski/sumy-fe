02a14b590e89d0c525b9801bf6f6dcaf8808de50
Toskyuu <robert.laski10@gmail.com>
2024-05-30 18:37:55 +0200

    Wywalenie klasy i zrobienie styled inputa

--- src/components/Elements/InputFields/DateTime/DateTime.tsx
+++ src/components/Elements/InputFields/DateTime/DateTime.tsx
@@ -10,47 +10,50 @@ export const DateTime = ({ label, error, id, ...rest }: DateTimeProps) => {
   return (
     <Container>
       <LabelText htmlFor={id}>{label}</LabelText>
-      <input className={'datePicker'} {...rest} type="datetime-local" id={id} name={id} />
+      <StyledDatePicker {...rest} type="datetime-local" id={id} name={id} />
       <StyledError>{error}</StyledError>
     </Container>
   );
 };
 
 const Container = styled.div`
-  ${({ theme }) => {
-    return css`
-      display: block;
-      margin-bottom: 1rem;
-      width: 100%;
-      font-size: 1rem;
-      font-weight: 600;
-      .datePicker{
-        width: 100%;
-          background-color: ${theme.colors.input};
-          font-size: ${theme.text['md']};
-          padding: ${theme.space[1]};
-          border: none;
-          border-bottom: 2px solid ${theme.colors.input};
-          border-radius: 0;
-    
-          &:focus,
-          &:hover {
-            border-color: ${theme.colors.buttons.primary};
-            outline: none;
-          }
-          &:invalid {
-            border-color: ${theme.colors.error};
-          }
-    
-          &::-webkit-calendar-picker-indicator {
-            cursor: pointer;
-        `;
-  }}
+  display: block;
+  margin-bottom: 1rem;
+  width: 100%;
+  font-size: 1rem;
+  font-weight: 600;
 `;
 
 const LabelText = styled.label`
   display: block;
   margin-bottom: 0.5rem;
+`;
+
+const StyledDatePicker = styled.input`
+  ${({ theme }) => {
+    return css`
+      width: 100%;
+      background-color: ${theme.colors.input};
+      font-size: ${theme.text['md']};
+      padding: ${theme.space[1]};
+      border: none;
+      border-bottom: 2px solid ${theme.colors.input};
+      border-radius: 0;
+
+      &:focus,
+      &:hover {
+        border-color: ${theme.colors.buttons.primary};
+        outline: none;
+      }
+      &:invalid {
+        border-color: ${theme.colors.error};
+      }
+
+      &::-webkit-calendar-picker-indicator {
+        cursor: pointer;
+      }
+    `;
+  }}
 `;
 
 const StyledError = styled.div`
