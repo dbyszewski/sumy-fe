26d75cb6cd4f5d1b7866cbbd4c0027615dc10762
Toskyuu <robert.laski10@gmail.com>
2024-05-30 16:02:01 +0200

    Ostylowany dateTimeField

--- src/components/Elements/InputFields/DateTime/DateTime.tsx
+++ src/components/Elements/InputFields/DateTime/DateTime.tsx
@@ -10,18 +10,42 @@ export const DateTime = ({ label, error, id, ...rest }: DateTimeProps) => {
   return (
     <Container>
       <LabelText htmlFor={id}>{label}</LabelText>
-      <input {...rest} type="datetime-local" id={id} name={id} />
+      <input className={'datePicker'} {...rest} type="datetime-local" id={id} name={id} />
       <StyledError>{error}</StyledError>
     </Container>
   );
 };
 
 const Container = styled.div`
-  display: block;
-  margin-bottom: 1rem;
-  width: 100%;
-  font-size: 1rem;
-  font-weight: 600;
+  ${({ theme }) => {
+    return css`
+      display: block;
+      margin-bottom: 1rem;
+      width: 100%;
+      font-size: 1rem;
+      font-weight: 600;
+      .datePicker{
+        width: 100%;
+          background-color: ${theme.colors.input};
+          font-size: ${theme.text['md']};
+          padding: ${theme.space[1]};
+          border: none;
+          border-bottom: 2px solid ${theme.colors.input};
+          border-radius: 0;
+    
+          &:focus,
+          &:hover {
+            border-color: ${theme.colors.buttons.primary};
+            outline: none;
+          }
+          &:invalid {
+            border-color: ${theme.colors.error};
+          }
+    
+          &::-webkit-calendar-picker-indicator {
+            cursor: pointer;
+        `;
+  }}
 `;
 
 const LabelText = styled.label`
