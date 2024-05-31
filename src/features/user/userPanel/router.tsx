4f064419fcc146c6e1c0f54261d4e1b9cd25d735
Toskyuu <robert.laski10@gmail.com>
2024-06-01 00:12:01 +0200

    przeniesienie routa karty pojedynczego eventu z admina do usera

--- src/features/user/userPanel/router.tsx
+++ src/features/user/userPanel/router.tsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 
 import { Layout } from './components/Layout.tsx';
 
+import { EventDetailsCard } from '@/features/admin/adminPanel/components/EventsTab/EventDetailsCard.tsx';
 import { EventsMapPanel } from '@/features/user/userPanel/pages/EventsMapPanel.tsx';
 import { UserEventsPanel } from '@/features/user/userPanel/pages/UserEventsPanel.tsx';
 import { UserSettingsPanel } from '@/features/user/userPanel/pages/UserSettingsPanel.tsx';
@@ -16,6 +17,7 @@ export const UserRouter = () => {
           <>
             <Route index element={<Navigate to="/user/events" />} />
             <Route path="/events" element={<UserEventsPanel />} />
+            <Route path="/events/:eventId" element={<EventDetailsCard />} />
             <Route path="/map" element={<EventsMapPanel />} />
             <Route path="/settings" element={<UserSettingsPanel />} />
             <Route path="*" element={<h1>404</h1>} />
