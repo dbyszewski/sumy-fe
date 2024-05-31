9c46e9ca8e3b41b9e4743e0438b3ea362f3a8f00
Toskyuu <robert.laski10@gmail.com>
2024-05-31 18:11:26 +0200

    zmiana ikon i wywalenie kolumny isPhoneVerified

--- src/utils/tableHelper.tsx
+++ src/utils/tableHelper.tsx
@@ -1,4 +1,13 @@
-import { faCheck, faXmark, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { faAdn } from '@fortawesome/free-brands-svg-icons';
+import {
+  faCheck,
+  faLock,
+  faUnlock,
+  faXmark,
+  faEye,
+  faEyeSlash,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { ReactNode } from 'react';
 import styled from 'styled-components';
@@ -22,6 +31,21 @@ export const renderEllipsis = (value: string) => {
 };
 export const renderVisibility = (value: boolean) => {
   return value ? <FontAwesomeIcon icon={faEye} /> : <FontAwesomeIcon icon={faEyeSlash} />;
+};
+export const renderAdmin = (value: ReactNode) => {
+  return value ? (
+    <StyledIcon icon={faAdn} value={!!value} />
+  ) : (
+    <StyledIcon icon={faUser} value={!!value} />
+  );
+};
+
+export const renderLocked = (value: ReactNode) => {
+  return value ? (
+    <StyledIcon icon={faLock} value={!value} />
+  ) : (
+    <StyledIcon icon={faUnlock} value={!value} />
+  );
 };
 
 const StyledIcon = styled(FontAwesomeIcon)<{ value: boolean }>`
