4c9f03e82310744df07057fc5bb048cfc91c6efb
dbyszewski <dbyszewski@gmail.com>
2024-06-01 21:02:05 +0200

    Add ReportButton Component
    A new 'ReportButton' component has been added with styling and functionality. It's imported and added to the 'AppLayout' component. It provides the ability to navigate to '/report' and hides when not in use.

---
+++ src/components/Elements/Button/ReportButton.tsx
@@ -0,0 +1,97 @@
+import { faFileCirclePlus, faAnglesRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { motion } from 'framer-motion';
+import { useCallback, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const ReportButton = () => {
+  const [isHidden, setIsHidden] = useState(false);
+  const navigate = useNavigate();
+  const handleClick = useCallback(() => {
+    navigate('/report');
+  }, [navigate]);
+
+  const reportVariants = {
+    hidden: {
+      x: 100,
+    },
+    visible: {
+      x: 0,
+    },
+  };
+
+  const hideButtonVariants = {
+    hidden: {
+      rotate: 180,
+    },
+    visible: {
+      rotate: 0,
+    },
+  };
+
+  return (
+    <Container>
+      <StyledButton
+        onClick={handleClick}
+        initial={isHidden ? 'visible' : 'hidden'}
+        animate={isHidden ? 'hidden' : 'visible'}
+        exit={isHidden ? 'visible' : 'hidden'}
+        whileHover={{ scale: 1.1 }}
+        transition={{ duration: 0.3 }}
+        variants={reportVariants}>
+        <FontAwesomeIcon icon={faFileCirclePlus} />
+      </StyledButton>
+      <HideButton
+        onClick={() => setIsHidden(!isHidden)}
+        initial={isHidden ? 'visible' : 'hidden'}
+        animate={isHidden ? 'hidden' : 'visible'}
+        exit={isHidden ? 'visible' : 'hidden'}
+        variants={hideButtonVariants}>
+        <FontAwesomeIcon icon={faAnglesRight} />
+      </HideButton>
+    </Container>
+  );
+};
+
+const StyledButton = styled(motion.button)`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  bottom: 3rem;
+  right: 2rem;
+  z-index: 50;
+  height: 6rem;
+  width: 6rem;
+  border-radius: 5rem;
+  background-color: ${({ theme }) => theme.colors.elements.darkRed};
+  border: 0.15rem solid ${({ theme }) => theme.colors.elements.light};
+  color: ${({ theme }) => theme.colors.text.light};
+  font-size: 2rem;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  cursor: pointer;
+`;
+
+const HideButton = styled(motion.button)`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  bottom: 9rem;
+  right: 0;
+  z-index: 50;
+  height: 2rem;
+  width: 2rem;
+  color: ${({ theme }) => theme.colors.elements.darkRed};
+  font-size: 2rem;
+  cursor: pointer;
+  background: none;
+  border: none;
+`;
+
+const Container = styled.div`
+  position: fixed;
+  height: 100vh;
+  width: 100vw;
+`;
--- src/components/Elements/Button/index.ts
+++ src/components/Elements/Button/index.ts
@@ -1,2 +1,3 @@
 export * from './Button';
 export * from './EmergencyButton';
+export * from './ReportButton';
